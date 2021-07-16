d2796b187ad2ed1845520f48b6c21d1676cbb0fe
al.dolgikh <al.dolgikh@tinkoff.ru>
2021-07-16 11:30:48 +0300

    x5

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int usernumber = 5;
+        Factorial f = new Factorial();
+        int out = f.fact(usernumber);
+        System.out.println(out);
+    }
+}
